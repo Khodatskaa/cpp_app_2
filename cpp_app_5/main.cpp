b3b5db8e12f0938c225a6cac98761f6f24b6f463
Khodatskaa <khodatskaalina@gmail.com>
2023-10-22 10:46:04 +0300

    Ex 3 was done

--- cpp_app_5/main.cpp
+++ cpp_app_5/main.cpp
@@ -2,5 +2,21 @@
 
 int main()
 {
+	double number;
 
+	std::cout << "Enter number: ";
+	std::cin >> number;
+
+	if (number > 0)
+	{
+		std::cout << "The number " <<number<< " is positive" << std::endl;
+	}
+	else if (number < 0)
+	{
+		std::cout << "The number " << number << " is negative" << std::endl;
+	}
+	else
+	{
+		std::cout << "The number is zero." << std::endl;
+	}
 }
